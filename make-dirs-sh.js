ec7fa01f402eddb672e86dc88dea177f709689f3
Ahmad Barclay <abarclay.ons@gmail.com>
2023-03-10 13:29:14 +0000

    add new hours_per_week_worked data

---
+++ make-dirs-sh.js
@@ -0,0 +1,18 @@
+import fs from "fs";
+
+const pmtiles_path = "./output/tiles/";
+const cmd = (key, file) => `tile-join -e output/dirs/${key} -pk output/tiles/${file}`;
+
+let datasets = [];
+
+// Find the .pmtiles files in the output directory
+fs.readdirSync(pmtiles_path)
+  .filter(file => file.slice(-8) === ".pmtiles")
+  .forEach(file => {
+    let key = file.slice(0, -8).split("-z")[0];
+    datasets.push({key, file});
+});
+
+let commands = datasets.map(d => cmd(d.key, d.file));
+
+fs.writeFileSync("./make-dirs.sh", commands.join("\n"));
