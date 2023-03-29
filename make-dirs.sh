0e2654ec216f260e051a469022a0785cc90d3e00
James Trimble <jtrim.ons@gmail.com>
2023-03-29 10:53:51 +0100

    Update scripts

--- make-dirs.sh
+++ make-dirs.sh
@@ -1,28 +1,56 @@
-tile-join -e output/dirs/accommodation_type_3a -pk output/tiles/accommodation_type_3a-z11.pmtiles
-tile-join -e output/dirs/country_of_birth_3a -pk output/tiles/country_of_birth_3a-z11.pmtiles
-tile-join -e output/dirs/disability_3a -pk output/tiles/disability_3a-z11.pmtiles
-tile-join -e output/dirs/economic_activity_status_4a -pk output/tiles/economic_activity_status_4a-z11.pmtiles
-tile-join -e output/dirs/english_proficiency_4a -pk output/tiles/english_proficiency_4a-z11.pmtiles
-tile-join -e output/dirs/ethnic_group_tb_6a -pk output/tiles/ethnic_group_tb_6a-z11.pmtiles
-tile-join -e output/dirs/has_ever_worked -pk output/tiles/has_ever_worked-z11.pmtiles
-tile-join -e output/dirs/health_in_general_4a -pk output/tiles/health_in_general_4a-z11.pmtiles
-tile-join -e output/dirs/heating_type_3a -pk output/tiles/heating_type_3a-z11.pmtiles
-tile-join -e output/dirs/hh_deprivation -pk output/tiles/hh_deprivation-z11.pmtiles
-tile-join -e output/dirs/hh_family_composition_4a -pk output/tiles/hh_family_composition_4a-z11.pmtiles
-tile-join -e output/dirs/hh_language -pk output/tiles/hh_language-z11.pmtiles
-tile-join -e output/dirs/hh_size_5a -pk output/tiles/hh_size_5a-z11.pmtiles
-tile-join -e output/dirs/hh_tenure_5a -pk output/tiles/hh_tenure_5a-z11.pmtiles
-tile-join -e output/dirs/highest_qualification_6a -pk output/tiles/highest_qualification_6a-z11.pmtiles
-tile-join -e output/dirs/hours_per_week_worked -pk output/tiles/hours_per_week_worked-z11.pmtiles
-tile-join -e output/dirs/in_full_time_education -pk output/tiles/in_full_time_education-z11.pmtiles
-tile-join -e output/dirs/is_carer_5a -pk output/tiles/is_carer_5a-z11.pmtiles
-tile-join -e output/dirs/legal_partnership_status_3a -pk output/tiles/legal_partnership_status_3a-z11.pmtiles
-tile-join -e output/dirs/migrant_ind -pk output/tiles/migrant_ind-z11.pmtiles
-tile-join -e output/dirs/national_identity_all_4a -pk output/tiles/national_identity_all_4a-z11.pmtiles
-tile-join -e output/dirs/number_bedrooms_5a -pk output/tiles/number_bedrooms_5a-z11.pmtiles
-tile-join -e output/dirs/number_of_cars_5a -pk output/tiles/number_of_cars_5a-z11.pmtiles
-tile-join -e output/dirs/occupancy_rating_bedrooms_6a -pk output/tiles/occupancy_rating_bedrooms_6a-z11.pmtiles
-tile-join -e output/dirs/passports_all_4a -pk output/tiles/passports_all_4a-z11.pmtiles
-tile-join -e output/dirs/residence_length_6b -pk output/tiles/residence_length_6b-z11.pmtiles
-tile-join -e output/dirs/sex -pk output/tiles/sex-z11.pmtiles
-tile-join -e output/dirs/workplace_travel_4a -pk output/tiles/workplace_travel_4a-z11.pmtiles
+tile-join -e output/dirs/accommodation_type_3a -pk output/tiles/accommodation_type_3a-z14.pmtiles
+(cd output/dirs/accommodation_type_3a && zip -r "../../dir-zips/accommodation_type_3a.zip" .)
+tile-join -e output/dirs/country_of_birth_3a -pk output/tiles/country_of_birth_3a-z14.pmtiles
+(cd output/dirs/country_of_birth_3a && zip -r "../../dir-zips/country_of_birth_3a.zip" .)
+tile-join -e output/dirs/disability_3a -pk output/tiles/disability_3a-z14.pmtiles
+(cd output/dirs/disability_3a && zip -r "../../dir-zips/disability_3a.zip" .)
+tile-join -e output/dirs/economic_activity_status_4a -pk output/tiles/economic_activity_status_4a-z14.pmtiles
+(cd output/dirs/economic_activity_status_4a && zip -r "../../dir-zips/economic_activity_status_4a.zip" .)
+tile-join -e output/dirs/english_proficiency_4a -pk output/tiles/english_proficiency_4a-z14.pmtiles
+(cd output/dirs/english_proficiency_4a && zip -r "../../dir-zips/english_proficiency_4a.zip" .)
+tile-join -e output/dirs/ethnic_group_tb_6a -pk output/tiles/ethnic_group_tb_6a-z14.pmtiles
+(cd output/dirs/ethnic_group_tb_6a && zip -r "../../dir-zips/ethnic_group_tb_6a.zip" .)
+tile-join -e output/dirs/has_ever_worked -pk output/tiles/has_ever_worked-z14.pmtiles
+(cd output/dirs/has_ever_worked && zip -r "../../dir-zips/has_ever_worked.zip" .)
+tile-join -e output/dirs/health_in_general_4a -pk output/tiles/health_in_general_4a-z14.pmtiles
+(cd output/dirs/health_in_general_4a && zip -r "../../dir-zips/health_in_general_4a.zip" .)
+tile-join -e output/dirs/heating_type_3a -pk output/tiles/heating_type_3a-z14.pmtiles
+(cd output/dirs/heating_type_3a && zip -r "../../dir-zips/heating_type_3a.zip" .)
+tile-join -e output/dirs/hh_deprivation -pk output/tiles/hh_deprivation-z14.pmtiles
+(cd output/dirs/hh_deprivation && zip -r "../../dir-zips/hh_deprivation.zip" .)
+tile-join -e output/dirs/hh_family_composition_4a -pk output/tiles/hh_family_composition_4a-z14.pmtiles
+(cd output/dirs/hh_family_composition_4a && zip -r "../../dir-zips/hh_family_composition_4a.zip" .)
+tile-join -e output/dirs/hh_language -pk output/tiles/hh_language-z14.pmtiles
+(cd output/dirs/hh_language && zip -r "../../dir-zips/hh_language.zip" .)
+tile-join -e output/dirs/hh_size_5a -pk output/tiles/hh_size_5a-z14.pmtiles
+(cd output/dirs/hh_size_5a && zip -r "../../dir-zips/hh_size_5a.zip" .)
+tile-join -e output/dirs/hh_tenure_5a -pk output/tiles/hh_tenure_5a-z14.pmtiles
+(cd output/dirs/hh_tenure_5a && zip -r "../../dir-zips/hh_tenure_5a.zip" .)
+tile-join -e output/dirs/highest_qualification_6a -pk output/tiles/highest_qualification_6a-z14.pmtiles
+(cd output/dirs/highest_qualification_6a && zip -r "../../dir-zips/highest_qualification_6a.zip" .)
+tile-join -e output/dirs/hours_per_week_worked -pk output/tiles/hours_per_week_worked-z14.pmtiles
+(cd output/dirs/hours_per_week_worked && zip -r "../../dir-zips/hours_per_week_worked.zip" .)
+tile-join -e output/dirs/in_full_time_education -pk output/tiles/in_full_time_education-z14.pmtiles
+(cd output/dirs/in_full_time_education && zip -r "../../dir-zips/in_full_time_education.zip" .)
+tile-join -e output/dirs/is_carer_5a -pk output/tiles/is_carer_5a-z14.pmtiles
+(cd output/dirs/is_carer_5a && zip -r "../../dir-zips/is_carer_5a.zip" .)
+tile-join -e output/dirs/legal_partnership_status_3a -pk output/tiles/legal_partnership_status_3a-z14.pmtiles
+(cd output/dirs/legal_partnership_status_3a && zip -r "../../dir-zips/legal_partnership_status_3a.zip" .)
+tile-join -e output/dirs/migrant_ind -pk output/tiles/migrant_ind-z14.pmtiles
+(cd output/dirs/migrant_ind && zip -r "../../dir-zips/migrant_ind.zip" .)
+tile-join -e output/dirs/national_identity_all_4a -pk output/tiles/national_identity_all_4a-z14.pmtiles
+(cd output/dirs/national_identity_all_4a && zip -r "../../dir-zips/national_identity_all_4a.zip" .)
+tile-join -e output/dirs/number_bedrooms_5a -pk output/tiles/number_bedrooms_5a-z14.pmtiles
+(cd output/dirs/number_bedrooms_5a && zip -r "../../dir-zips/number_bedrooms_5a.zip" .)
+tile-join -e output/dirs/number_of_cars_5a -pk output/tiles/number_of_cars_5a-z14.pmtiles
+(cd output/dirs/number_of_cars_5a && zip -r "../../dir-zips/number_of_cars_5a.zip" .)
+tile-join -e output/dirs/occupancy_rating_bedrooms_6a -pk output/tiles/occupancy_rating_bedrooms_6a-z14.pmtiles
+(cd output/dirs/occupancy_rating_bedrooms_6a && zip -r "../../dir-zips/occupancy_rating_bedrooms_6a.zip" .)
+tile-join -e output/dirs/passports_all_4a -pk output/tiles/passports_all_4a-z14.pmtiles
+(cd output/dirs/passports_all_4a && zip -r "../../dir-zips/passports_all_4a.zip" .)
+tile-join -e output/dirs/residence_length_6b -pk output/tiles/residence_length_6b-z14.pmtiles
+(cd output/dirs/residence_length_6b && zip -r "../../dir-zips/residence_length_6b.zip" .)
+tile-join -e output/dirs/sex -pk output/tiles/sex-z14.pmtiles
+(cd output/dirs/sex && zip -r "../../dir-zips/sex.zip" .)
+tile-join -e output/dirs/workplace_travel_4a -pk output/tiles/workplace_travel_4a-z14.pmtiles
+(cd output/dirs/workplace_travel_4a && zip -r "../../dir-zips/workplace_travel_4a.zip" .)
