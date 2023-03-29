06ae4b0537041d83be2c811d87f2df4fff396772
James Trimble <jtrim.ons@gmail.com>
2023-03-29 10:55:32 +0100

    Add to run-everything script

--- run-everything.sh
+++ run-everything.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 mkdir -p output/dots
 mkdir -p output/dirs
 mkdir -p output/dir-zips
+mkdir -p python-test/results
 
 npm install
 
@@ -18,13 +19,23 @@ echo Running make-dotsets.js...
 node make-dotsets
 echo Running make-pmtiles-sh.js...
 node make-pmtiles-sh
+
 echo Running make-pmtiles.sh...
+chmod u+x make-pmtiles.sh
+rm -f output/tiles/*
 ./make-pmtiles.sh
 echo Running make-dirs-sh.js...
 node make-dirs-sh
 echo Running make-dirs.sh...
+chmod u+x make-dirs.sh
 ./make-dirs.sh
 echo Running make-quads.js...
 node make-quads
 
+echo Running check-dotcounts.py...
+(cd python-test/ && python3 check-dotcounts.py | tee results/check-dotcounts-output.txt)
+echo Running recompute-dotcounts.py...
+(cd python-test/ && python3 recompute-dotcounts.py | tee results/recompute-dotcounts-output.txt)
+
 echo Done.
+echo To do manually: check python-test results.
